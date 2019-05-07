4a2af1267d0f3f342dcd33681e3ed379ae53f4cb
nhellou <nora.hellou@gmail.com>
2019-05-07 17:33:00 +0200

    creation partie ordre de commande front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {  HttpClientModule } from '@angular/common/http';
+import { RoutModule } from './rout/rout.module';
 
 import { AppComponent } from './app.component';
+import { NewOrdreCoupeComponent } from './new-ordre-coupe/new-ordre-coupe.component';
+import { ListeOrdreCoupeComponent } from './liste-ordre-coupe/liste-ordre-coupe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewOrdreCoupeComponent,
+    ListeOrdreCoupeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/IProduit.ts
@@ -0,0 +1,9 @@
+export interface Produit {
+    code : number;
+    fabricant : string;
+    taille : string;
+    couleur : string;
+    modele : string;
+    prix : number;
+    quantite: number;
+}
