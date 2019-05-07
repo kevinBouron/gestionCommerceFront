c977845e2f63f0981da3588c2a27d7ce9e36a5c5
laurieannejouanno <laurieannejouanno@gmail.com>
2019-05-07 17:19:10 +0200

    partie reglement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ReglementComponent } from './reglement/reglement.component';
+import { ReglementService } from './reglement.service';
+import {  HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReglementComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ ReglementService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
